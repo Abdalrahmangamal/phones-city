8d0766aee295f3c3caf40fe6f6d418fac20c6811
ErenyThrwat27 <ereny.thrwat27@gmail.com>
2025-12-20 20:02:40 +0200

    eee

--- src/components/chekout/Checkoutsummary.tsx
+++ src/components/chekout/Checkoutsummary.tsx
@@ -1,17 +1,14 @@
 import Ordersummary from "./public/Ordersummary";
 import Basketsummary from "./summary/Basketsummary";
-import { useState } from "react";
 
 export default function Cheackoutsummary({products,total}: any) {
-  const [finalTotal, setFinalTotal] = useState(total);
-
   return (
-    <div className="grid xl:px-[90px] px-[0px] grid-cols-1 md:grid-cols-2">
+    <div className="grid  xl:px-[90px] px-[0px] grid-cols-1 md:grid-cols-2">
       <div className="lg:pl-[40px] md:pl-[10px]">
-        <Basketsummary products={products} total={finalTotal}/> 
+        <Basketsummary  products={products} total={total}/> 
       </div>
       <div>
-        <Ordersummary onTotalUpdate={setFinalTotal} />
+        <Ordersummary />
       </div>
     </div>
   );
